caf4a805c4da0515b75e43d1b78d351c468f2bd4
Sydney Jupiter <sydneyjupiters@gmail.com>
2022-01-26 21:33:37 +0000

    finished all styles

--- script.js
+++ script.js
@@ -1,62 +1,50 @@
-const bodyPage = document.querySelector('body');
-const openMenuButton = document.querySelector('.ph-list-thin');
-const closeMenuButton = document.querySelector('.ph-x-thin');
-const menuItems = document.querySelector('.menu-items');
-const menuLinkOne = document.querySelector('.nl1');
-const menuLinkTwo = document.querySelector('.nl2');
-const menuLinkThree = document.querySelector('.nl3');
-const menuLinkFour = document.querySelector('.nl4');
-const menuLinkFive = document.querySelector('.nl5');
-const menuLinkSix = document.querySelector('.nl6');
+const bodyPage = document.querySelector("body");
+const closeMenuButton = document.querySelector(".ph-x-thin");
+const menuItems = document.querySelector(".menu-items");
+const openMenuButton = document.querySelector(".ph-list-thin");
 
 /* Mobile navigation menu appears when you click the menu button */
 
-openMenuButton.addEventListener('click',()=> {
-    menuItems.classList.add('menu-items-appear');
-    closeMenuButton.classList.add('close-menu-appears');
-    openMenuButton.classList.add('menu-button-disappears');
-    bodyPage.classList.add('body-stop-scrolling');
+openMenuButton.addEventListener("click", () => {
+  bodyPage.classList.add("body-stop-scrolling");
+  closeMenuButton.classList.add("close-menu-visible");
+  menuItems.classList.add("menu-items-visible");
+  openMenuButton.classList.add("menu-button-invisible");
 });
 
 /* Mobile navigation menu disappears when you click the X button */
 
 function closeMenu() {
-    menuItems.classList.remove('menu-items-appear');
-    closeMenuButton.classList.remove('close-menu-appears');
-    openMenuButton.classList.remove('menu-button-disappears');
-    bodyPage.classList.remove('body-stop-scrolling');
+  bodyPage.classList.remove("body-stop-scrolling");
+  closeMenuButton.classList.remove("close-menu-visible");
+  menuItems.classList.remove("menu-items-visible");
+  openMenuButton.classList.remove("menu-button-invisible");
 }
 
-closeMenuButton.addEventListener('click',()=> {
-    closeMenu();
+closeMenuButton.addEventListener("click", () => {
+  closeMenu();
 });
 
-/* Mobile navigation menu disappears when you click individual menu links */
+/* Mobile navigation menu disappears when you click menu items */
 
-menuLinkOne.addEventListener('click',()=> {
-    closeMenu();
+menuItems.addEventListener("click", () => {
+  closeMenu();
 });
 
-menuLinkTwo.addEventListener('click',()=> {
-    closeMenu();
-});
+/* Random image on page load */
 
-menuLinkThree.addEventListener('click',()=> {
-    closeMenu();
-});
+const images = [
+  '/assets/images/team-photo-one.jpg',
+  '/assets/images/team-photo-two.jpg',
+  '/assets/images/team-photo-three.jpg',
+  '/assets/images/team-photo-four.jpg'
+];
 
-menuLinkFour.addEventListener('click',()=> {
-    closeMenu();
-});
+const imageDestination = document.querySelector('.hero-subsection');
 
-menuLinkFive.addEventListener('click',()=> {
-    closeMenu();
-});
+function displayImage(num) {
+   num = Math.floor(Math.random() * 4);
+   imageDestination.style.backgroundImage = `url("${images[num]}")`;
+};
 
-menuLinkSix.addEventListener('click',()=> {
-    closeMenu();
-});
-
-
-
-
+displayImage();
